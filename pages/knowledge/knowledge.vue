00502373d31c120b030f49f3b57ccbbb082b944f
Hsiwozer <1377311206@qq.com>
2022-09-21 16:10:57 +0800

    commit tabbar

---
+++ pages/knowledge/knowledge.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    knowledge
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
