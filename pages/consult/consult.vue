dc113f40051731120eabdbe63b27dcb9cf611fc1
Hsiwozer <1377311206@qq.com>
2022-12-08 22:41:50 +0800

    add new button

--- pages/consult/consult.vue
+++ pages/consult/consult.vue
@@ -11,11 +11,15 @@
         </uni-card>
       </uni-section>
       
-        <div class="tagView" v-for="(item1, index1) in syndromeList" :key="index1" v-if="item1.id === page">
-          <uni-tag v-for="(item2, index2) in item1.elements" :key="index2" inverted="true" :text="item2" type="primary" circle="true" ref="utag" @click.stop="changeStatus(index2)" />
-        </div>
-
-      <button type="primary" class="btn" @click="btnHandler">下一组</button>
+        <view class="tagView" v-for="(item1, index1) in syndromeList" :key="index1" v-if="item1.id === page">
+          <uni-tag v-for="(item2, index2) in item1.elements" :key="index2" :inverted="true" :text="item2" type="primary" circle="true" ref="utag" @click="setInverted(index2)" />
+        </view>
+        
+      <view class="btnBox">
+        <button type="primary" class="btn" @click="btnHandlerPrevious" v-if="page !== 1">上一组</button>
+        <button type="primary" class="btn" @click="btnHandlerNext" v-if="page !== 8">下一组</button>
+        <button type="primary" class="btn" v-if="page === 8">完成</button>
+      </view>
     </view>
     
   </view>
@@ -27,7 +31,7 @@
     data() {
       return {
         isShowPage: false,
-        page: 1
+        page: 1,
       };
     },
     computed: {
@@ -39,12 +43,15 @@
     },
     methods: {
       ...mapMutations('m_syndromes', ['updateSyndromes']),
-      btnHandler() {
+      btnHandlerPrevious() {
+        if(this.page != 1) this.page -= 1
+      },
+      btnHandlerNext() {
         if(this.page < 8) this.page += 1
       },
-      changeStatus(index) {
-        this.$refs.utag[index].inverted = !this.$refs.utag[index].inverted
-      }
+      setInverted(i) {
+        this.$refs.utag[i].inverted = !this.$refs.utag[i].inverted
+      },
     }
   }
 </script>
@@ -64,19 +71,23 @@
           margin: 25rpx;
         }
       }
-      
-      .btn {
+      .btnBox {
         width: 50%;
-        height: 48px;
-        background: rgb(0,163,255);
-        background: linear-gradient(90deg, rgba(0,165,255,1) 30%, rgba(0,105,255,1) 100%);
-        border-radius: 10px;
-        margin: 0 auto;
         position: fixed;
         bottom: 100rpx;
         left: calc(50%);
         transform: translateX(-50%);
+        
+        .btn {
+          width: 100%;
+          height: 48px;
+          background: rgb(0,163,255);
+          background: linear-gradient(90deg, rgba(0,165,255,1) 30%, rgba(0,105,255,1) 100%);
+          border-radius: 10px;
+          margin: 10px auto;
+        }
       }
+      
     }
   }
 </style>
